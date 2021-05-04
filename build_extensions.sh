f1be62ecf518735b9d4406046ebdf4a132c08947
aintitfun <opolofdez@gmail.com>
2021-05-04 13:01:16 +0200

    loop extensions throug a file

--- build_extensions.sh
+++ build_extensions.sh
@@ -1,13 +1,17 @@
-case "$2" in
-    "")
-	rm -rf odbc_fdw
-	rm -rf postgres_sources/${1}/postgres/contrib/odbc_fdw
-	git clone git@github.com:aintitfun/odbc_fdw.git
-	;;
-    "--no-download")
-	;;
-esac
-mv odbc_fdw postgres_sources/${1}/postgres/contrib
-cd postgres_sources/${1}/postgres/contrib/odbc_fdw
-make && make install
+rootdir=`pwd`
+while read line; do
+	case "$2" in
+	    "")
+		rm -rf ${line}
+		rm -rf postgres_sources/${1}/postgres/contrib/${line}
+		git clone git@github.com:aintitfun/${line}.git
+		;;
+	    "--no-download")
+		;;
+	esac
+	mv ${line} postgres_sources/${1}/postgres/contrib
+	cd postgres_sources/${1}/postgres/contrib/${line}
+	make && make install
+	cd $rootdir
+done <list_of_extensions.txt
 
