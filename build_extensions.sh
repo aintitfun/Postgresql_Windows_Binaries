a745916c81bb80aa7eea35d5f0d88479313f48f3
aintitfun <opolofdez@gmail.com>
2021-05-04 08:28:58 +0200

    set --no-download parameter on build_extensions.sh

--- build_extensions.sh
+++ build_extensions.sh
@@ -1,6 +1,13 @@
-rm -rf odbc_fdw
-git clone git@github.com:aintitfun/odbc_fdw.git
-cp -R odbc_fdw/ postgres_sources/${1}/postgres/contrib
+case "$2" in
+    "")
+	rm -rf odbc_fdw
+	rm -rf postgres_sources/${1}/postgres/contrib/odbc_fdw
+	git clone git@github.com:aintitfun/odbc_fdw.git
+	;;
+    "--no-download")
+	;;
+esac
+mv odbc_fdw postgres_sources/${1}/postgres/contrib
 cd postgres_sources/${1}/postgres/contrib/odbc_fdw
-make
+make && make install
 
